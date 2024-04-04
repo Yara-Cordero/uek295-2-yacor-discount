1a9ac57d60f8277b1c2e743474c9321c5a040d50
Yara-Cordero <145566014+Yara-Cordero@users.noreply.github.com>
2024-04-04 13:35:05 +0200

    setup project environment

---
+++ uek295yacordiscount/src/main/java/ch/noseryoung/uek295yacordiscount/Uek295yacordiscountApplication.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.uek295yacordiscount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uek295yacordiscountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uek295yacordiscountApplication.class, args);
+	}
+
+}
---
+++ uek295yacordiscount/src/test/java/ch/noseryoung/uek295yacordiscount/Uek295yacordiscountApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.uek295yacordiscount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uek295yacordiscountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
