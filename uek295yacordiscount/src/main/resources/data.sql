ba4a5411ed45b0955f158dd7543f110f71cc11dc
Yara-Cordero <145566014+Yara-Cordero@users.noreply.github.com>
2024-04-17 14:54:53 +0200

    added data.sql

---
+++ uek295yacordiscount/src/main/resources/data.sql
@@ -0,0 +1,48 @@
+INSERT INTO discounts ("discount_id","code", "discount", "id_client")
+VALUES (
+        default,
+        'HB4FG28',
+        25,
+        04684
+       ),
+       (
+        default,
+        'KWN9SWU',
+        10,
+        24123
+       ),
+       (
+        default,
+        'Q12M0SV',
+        50,
+        03933
+       ),
+       (
+        default,
+        'BV003YP',
+        30,
+        18294
+       )
+ON CONFLICT DO NOTHING;
+
+INSERT INTO authorities("authority_id", "authority_name")
+VALUES (1, 'READ'),
+       (2, 'CREATE'),
+       (3, 'UPDATE'),
+       (4, 'DELETE')
+ON CONFLICT DO NOTHING;
+INSERT INTO roles("role_id", "role_name")
+VALUES (1, 'ADMIN'),
+       (2, 'USER')
+ON CONFLICT DO NOTHING;
+INSERT INTO roles_authorities("id_role", "id_authority")
+VALUES (1, 1),
+       (1, 2),
+       (1, 3),
+       (1, 4),
+       (2, 1)
+ON CONFLICT DO NOTHING;
+INSERT INTO users("password", "name", "user_role")
+VALUES ('123', 'cooldude22', 1),
+       ('123', 'tubularperson10', 2)
+ON CONFLICT DO NOTHING;
